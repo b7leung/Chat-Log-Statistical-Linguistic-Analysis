f906471ba30547fae8bf533b085bd4a41bf5dfe2
Shiv <58155330+shivadb@users.noreply.github.com>
2021-05-13 11:51:45 -0700

    Initial Dev

---
+++ nlp_suite/clustering/compute_tfidf.py
@@ -0,0 +1,31 @@
+'''
+Compute TFIDF encodings for training data
+'''
+
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import argparse
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+
+parser = argparse.ArgumentParser(description='Compute feature vectors')
+parser.add_argument('--datapath', type=Path, required=True)
+parser.add_argument('--minfreq', type=int, default=10)
+parser.add_argument('--maxfeat', type=int, default=10000)
+parser.add_argument('--fname', type=str, default='encoded_data.pkl')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    df = pd.read_csv(args.datapath, converters={'Chats': eval})
+    df['Chats'] = df['Chats'].apply(' '.join)
+    vectorizer = TfidfVectorizer(min_df = args.minfreq, 
+                                max_features = args.maxfeat,  
+                                token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b')
+    tfidf_vectors = vectorizer.fit_transform(df['Chats'])
+
+    with open(args.fname, 'wb') as f:
+        pickle.dump({'dataframe': df,
+                    'vectorizer': vectorizer,
+                    'encodings': tfidf_vectors},
+                    f)
---
+++ nlp_suite/clustering/utils.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.cluster import MiniBatchKMeans
+from collections.abc import Iterable
+
+def plot_3d_clusters(pca_proj, labels, max_points=10000, point_size=2):
+    '''
+    Plot 3D pca projections on a plotly graph
+    '''
+    idx = np.random.choice(range(pca_proj.shape[0]), size=max_points, replace=False)
+
+    fig = go.FigureWidget([go.Scatter3d(x=pca_proj[idx, 0],
+                                 y=pca_proj[idx, 1],
+                                 z=pca_proj[idx, 2], 
+                                 mode='markers',
+                                 marker=dict(size=point_size,
+                                             color=labels[idx],
+                                             colorscale='Rainbow'),
+                                 text=[f'Cluster {l}' for l in labels[idx]],
+                                 hoverinfo='text'
+                                )])
+
+    fig.layout.update(scene=dict(
+                      xaxis=dict(showticklabels=False, title='', showspikes=False),
+                      yaxis=dict(showticklabels=False, title='', showspikes=False),
+                      zaxis=dict(showticklabels=False, title='', showspikes=False),
+                                ))
+
+    return fig
+
+
+def classify_text(vectorizer, clusters, text_docs):
+    '''
+    Classify documents of text given a learned vectorizer and cluster centers
+    '''
+    assert isinstance(vectorizer, TfidfVectorizer)
+    assert isinstance(clusters, MiniBatchKMeans)
+    text_docs = [text_docs] if isinstance(text_docs, str) else text_docs
+    assert isinstance(text_docs, Iterable)
+
+    return clusters.predict(vectorizer.transform(text_docs))
