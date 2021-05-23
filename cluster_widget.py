675309ff63c99dd34c51e9a55f0139e1ab1a928c
kevinyoussef <kryousse@ucsd.edu>
2021-05-23 03:13:53 +0200

    Added text analysis plots for clustering tab (broken tho)

--- cluster_widget.py
+++ cluster_widget.py
@@ -1,6 +1,12 @@
 import ipywidgets as widgets
+from ipywidgets import Checkbox, VBox, HBox, HTML, Dropdown, Image, IntSlider
 import pickle
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from nlp_suite.clustering.utils import plot_3d_clusters, classify_text
+from nlp_suite.text_analysis.text_analysis_kevin import *
 
 # widgets for the dashboard follow a similar pattern, as follows
 # note the get_widget and init_widget_data methods.
@@ -10,11 +16,11 @@ class ClusterWidget():
     # that its elements can be updated when new data comes in.
     # however, it should not rely on the user_info yet, because that is not available when the app first starts.
     def __init__(self):
-        self.widget = widgets.Text(description="3D visualization of user clusters")
+        
         pkl_data = pickle.load(open('./nlp_suite/clustering/cluster_data.pkl', 'rb'))
         labels, pca = pkl_data['labels'], pkl_data['pca']
         self.fig = plot_3d_clusters(pca, labels, max_points=50000)
-        self.cluster_dd = widgets.Dropdown(
+        self.cluster_dd = Dropdown(
                 options=[(f'Cluster {i}', i) for i in range(max(labels)+1)],
                 value=0,
                 description='Cluster:',
@@ -24,7 +30,208 @@ class ClusterWidget():
             
         self.fig.data[0].on_click(on_click)
 
-        self.widget = widgets.VBox([self.fig, self.cluster_dd])
+        self.hi=False
+        self.here = False
+        self.here2 = False
+        self.here3 = False
+        user_messages_path='/Users/kevinyoussef/Desktop/ECE 229/Project/ece_229_group_9_nlp_suite/cached_user_data/Iinden/user_messages.p'
+        df = pd.DataFrame()
+        df['user_messages'] = pickle.load(open(user_messages_path, "rb"))
+        self.text_analysis = get_text_analysis(df)
+        get_plots(*self.text_analysis)
+        
+        Graphs = [
+            'Message Length Histogram',
+            'Average Word Length Histogram',
+            'Top 10 Stop Words Histogram',
+            'Top 10 Unigrams Histogram',
+            'Top 10 Bigrams Histogram',
+            'Top 10 Trigrams Histogram',
+            'Word Cloud'
+        ]
+
+        plot_names = [
+            'message_lengths_hist.png',
+            'average_word_lengths_hist.png',
+            'stop_dic_histogram.png',
+            'unigrams.png',
+            'bigrams.png',
+            'trigrams.png',
+            'word_cloud.png',
+        ]
+
+        self.cb_all = Checkbox(description='All Graphs')
+        self.cb0 = Checkbox(description=Graphs[0])
+        self.cb1 = Checkbox(description=Graphs[1])
+        self.cb2 = Checkbox(description=Graphs[2])
+        self.cb3 = Checkbox(description=Graphs[3])
+        self.cb4 = Checkbox(description=Graphs[4])
+        self.cb5 = Checkbox(description=Graphs[5])
+        self.cb6 = Checkbox(description=Graphs[6])
+        self.plots_list = [0,0,0,0,0,0,0]
+        widgets_dict = {}
+
+        def cb_all_show(button):
+            if not button['new']:
+                self.hi = True
+                self.cb3.value=False
+                self.cb0.value=False
+                self.cb1.value=False
+                self.cb2.value=False
+                self.cb4.value=False
+                self.cb5.value=False
+                self.cb6.value=False
+            else:
+                self.cb0.value=True
+                self.cb1.value=True
+                self.cb2.value=True
+                self.cb3.value=True
+                self.cb4.value=True
+                self.cb5.value=True
+                self.cb6.value=True
+                self.here3 = True
+
+        def cb0_show(button):
+            key = 'plot0'
+            if button['new']:
+                widgets_dict[key] = self.plots_list[0]
+                self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+            else:
+                try:
+                    del widgets_dict[key]
+                    self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                except KeyError:
+                    print("ERROR")
+                    pass
+
+        def cb1_show(button):
+            key = 'plot1'
+            if button['new']:
+                widgets_dict[key] = self.plots_list[1]
+                self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+            else:
+                try:
+                    del widgets_dict[key]
+                    self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                except KeyError:
+                    print("ERROR")
+                    pass
+
+        def cb2_show(button):
+            key = 'plot2'
+            if button['new']:
+                widgets_dict[key] = self.plots_list[2]
+                self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+            else:
+                try:
+                    del widgets_dict[key]
+                    self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                except KeyError:
+                    print("ERROR")
+                    pass
+
+        def cb3_show(button):
+            
+            key = 'plot3'
+            if button['new']:
+                widgets_dict[key] = self.plots_list[3]
+                self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                self.here = True
+            else:
+                try:
+                    del widgets_dict[key]
+                    self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                except KeyError:
+                    print("ERROR")
+                    pass
+
+        def cb4_show(button):
+            key = 'plot4'
+            if button['new']:
+                widgets_dict[key] = self.plots_list[4]
+                self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+            else:
+                try:
+                    del widgets_dict[key]
+                    self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                except KeyError:
+                    print("ERROR")
+                    pass
+
+        def cb5_show(button):
+            key = 'plot5'
+            if button['new']:
+                widgets_dict[key] = self.plots_list[5]
+                self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+            else:
+                try:
+                    del widgets_dict[key]
+                    self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                except KeyError:
+                    print("ERROR")
+                    pass
+
+        def cb6_show(button):
+            key = 'plot6'
+            if button['new']:
+                widgets_dict[key] = self.plots_list[6]
+                self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+            else:
+                try:
+                    del widgets_dict[key]
+                    self.vb.children = [*self.checkboxes,*widgets_dict.values()]
+                except KeyError:
+                    print("ERROR")
+                    pass
+
+        def set_plot_sizes(x):
+            for i in range(len(Graphs)):
+                self.here2 = True
+                plot = open(plot_names[i], "rb")
+                image = plot.read()
+                self.plots_list[i] = Image(value=image,format='png',width=x)
+            cb0_show({'new':self.cb0.value})
+            cb1_show({'new':self.cb1.value})
+            cb2_show({'new':self.cb2.value})
+            cb3_show({'new':self.cb3.value})
+            cb4_show({'new':self.cb4.value})
+            cb5_show({'new':self.cb5.value})
+            cb6_show({'new':self.cb6.value})
+
+        self.slider = IntSlider(
+            value=300,
+            min=100,
+            max=1000,
+            step=10,
+            description='Size Of Plots:',
+            disabled=False,
+            continuous_update=False,
+            orientation='horizontal',
+            readout=True,
+            readout_format='d'
+        )
+
+        self.vb = VBox([self.slider,
+            HBox([self.cb_all, self.cb0, self.cb1, self.cb2]), 
+            HBox([self.cb3, self.cb4, self.cb5, self.cb6])
+            ])
+        self.checkboxes = self.vb.children
+
+        self.slider.observe(set_plot_sizes, names='values')
+        self.cb_all.observe(cb_all_show, names='value')
+        self.cb0.observe(cb0_show, names='value')
+        self.cb1.observe(cb1_show, names='value')
+        self.cb2.observe(cb2_show, names='value')
+        self.cb3.observe(cb3_show, names='value')
+        self.cb4.observe(cb4_show, names='value')
+        self.cb5.observe(cb5_show, names='value')
+        self.cb6.observe(cb6_show, names='value')
+
+        layout = widgets.Layout(width='auto')
+        header = HTML(description="3D visualization of user clusters",layout=layout)
+        self.widget = VBox([header, self.fig, self.cluster_dd, self.vb])
+
+    
 
 
     # returns the widget skeleton. this is used when the dashboard is first displayed since 
---
+++ nlp_suite/text_analysis/text_analysis_kevin.py
@@ -0,0 +1,158 @@
+import nltk
+from nltk import word_tokenize
+from nltk.util import ngrams
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem.wordnet import WordNetLemmatizer
+from collections import defaultdict, Counter
+from sklearn.base import clone
+from sklearn.feature_extraction.text import CountVectorizer
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+def get_text_analysis(df):
+    '''
+    Get Stop Words histogram, Unigrams, Bigrams, Trigra, and Word Cloud
+    Input: 
+    df: Datatframe with a column labeled 'user_messages' 
+    
+    Outputs: 
+    message_lengths: pd.Series of each message's length in terms of words
+    average_word_length: pd.Series of each message's average word length
+    stop_dic : collections.defaultdict
+    unigrams: decending ordered by frequency list of tuples: (unigram, frequency)
+    bigrams: decending ordered by frequency list of tuples: (bigram, frequency)
+    trigrams: decending ordered by frequency list of tuples: (trigram, frequency)
+    wordcloud: word cloud function, plot using plt.imshow
+    '''
+    nltk.download('stopwords')
+    stop=set(stopwords.words('english'))
+    corpus=[]
+    new= df['user_messages'].str.split()
+    new=new.values.tolist()
+    corpus=[word for i in new for word in i]
+    
+    stop_dic=defaultdict(int)
+    for word in corpus:
+        if word in stop:
+            stop_dic[word]+=1
+
+    counter=Counter(corpus)
+    most=counter.most_common()
+    x, y= [], []
+    for word,count in most[:40]:
+        if (word not in stop):
+            x.append(word)
+            y.append(count)
+    corpus = pd.Series(preprocess_news(df))
+    corpus = corpus.apply(lambda x:' '.join(x))
+    unigrams=get_top_ngram(corpus,n=1)
+    bigrams=get_top_ngram(corpus,n=2)
+    trigrams=get_top_ngram(corpus,n=3)
+    wordcloud = get_wordcloud(corpus)
+    message_lengths = df['user_messages'].str.len()
+    average_word_lengths = df['user_messages'].str.split().apply(lambda x : [len(i) for i in x]).map(lambda x: np.mean(x))
+    return message_lengths,average_word_lengths,stop_dic,unigrams,bigrams,trigrams,wordcloud
+
+def get_top_ngram(corpus, n=None):
+    '''
+    Find ngram frequency in descending order
+    '''
+    vec = CountVectorizer(ngram_range=(n, n)).fit(corpus)
+    bag_of_words = vec.transform(corpus)
+    sum_words = bag_of_words.sum(axis=0) 
+    words_freq = [(word, sum_words[0, idx]) 
+                  for word, idx in vec.vocabulary_.items()]
+    words_freq =sorted(words_freq, key = lambda x: x[1], reverse=True)
+    return words_freq
+
+def get_wordcloud(data):
+    stopwords = set(STOPWORDS)
+    wordcloud = WordCloud(
+        background_color='black',
+        stopwords=stopwords,
+        max_words=100,
+        max_font_size=30,
+        scale=3,
+        random_state=1)
+   
+    wordcloud=wordcloud.generate(str(data))
+    return wordcloud
+
+def plot_message_lengths_hist(message_lengths):
+    plt.hist(message_lengths,bins = int(max(message_lengths)//10))
+    plt.xlim([0,1000])
+    plt.title('Message Lengths Histogram')
+    plt.savefig('message_lengths_hist.png')
+
+def plot_average_word_lengths(average_word_lengths):
+    plt.hist(average_word_lengths,bins = int(max(average_word_lengths)))
+    plt.title('Average Word Length Histogram')
+    plt.savefig('average_word_lengths_hist.png')
+
+def plot_stop_dic(stop_dic,top_n=10):
+    idx = np.argsort(list(stop_dic.values()))[::-1]
+    words = np.array(list(stop_dic.keys()))
+    freq = np.array(list(stop_dic.values()))
+    plt.bar(words[idx[:top_n]], freq[idx[:top_n]])
+    plt.title('Top 10 Stop Words Histogram')
+    plt.savefig('stop_dic_histogram.png')
+
+def plot_top_n_ngrams(ngrams,n=10):
+    x,y = zip(*ngrams)
+    plt.barh(list(x[:n]),list(y[:n]))
+
+def preprocess_news(df):
+    stop=set(stopwords.words('english'))
+    nltk.download('punkt')
+    nltk.download('wordnet')
+    corpus=[]
+    stem=PorterStemmer()
+    lem=WordNetLemmatizer()
+    for news in df['user_messages']:
+        words=[w for w in word_tokenize(news) if (w not in stop)]
+        
+        words=[lem.lemmatize(w) for w in words if len(w)>2]
+        
+        corpus.append(words)
+    return corpus
+
+def plot_word_cloud(wordcloud):
+    plt.imshow(wordcloud)
+    plt.title('Word Cloud')
+    plt.axis('off')
+    plt.savefig('word_cloud.png')
+
+def plot_unigrams(unigrams):
+    plot_top_n_ngrams(unigrams)
+    plt.title('Top 10 Unigrams Histogram')
+    plt.savefig('unigrams.png')
+
+def plot_bigrams(bigrams):
+    plot_top_n_ngrams(bigrams)
+    plt.title('Top 10 Bigrams Histogram')
+    plt.savefig('bigrams.png')
+
+def plot_trigrams(trigrams):
+    plot_top_n_ngrams(trigrams)
+    plt.title('Top 10 Trigrams Histogram')
+    plt.savefig('trigrams.png')
+
+def get_plots(message_lengths,average_word_lengths,stop_dic,unigrams,bigrams,trigrams,wordcloud):
+    plt.figure()
+    plot_word_cloud(wordcloud)
+    plt.figure()
+    plot_message_lengths_hist(message_lengths)
+    plt.figure()
+    plot_unigrams(unigrams)
+    plt.figure()
+    plot_average_word_lengths(average_word_lengths)
+    plt.figure()
+    plot_bigrams(bigrams)
+    plt.figure()
+    plot_stop_dic(stop_dic)
+    plt.figure()
+    plot_trigrams(trigrams)
+    plt.figure()
